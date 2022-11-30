82459dbc12af97ada6f39272cd0cb5ba35cbebac
hugo chaumette <hugochaumette@gmail.com>
2022-11-30 11:41:14 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
+Exercise.destroy_all
+
+url = URI("https://exercisedb.p.rapidapi.com/exercises")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(url)
+request["X-RapidAPI-Key"] = 'cbd2cc8781msh965ef8f274c9a90p197ee1jsn8f68e546e7db'
+request["X-RapidAPI-Host"] = 'exercisedb.p.rapidapi.com'
+
+response = http.request(request)
+
+exercises = JSON.parse(response.read_body)
+
+exercises.first(10).each do |exercise|
+  Exercise.create!(
+    title: exercise["name"],
+    equipment: exercise["equipment"],
+    demonstration: exercise["gifUrl"],
+    body_part: exercise["bodyPart"]
+  )
+end
+
+# t.integer "duration"
+# t.integer "rest_time"
+# t.integer "repetitions"
+# t.string "demonstration"
+# t.string "title"
+# t.string "equipment"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
