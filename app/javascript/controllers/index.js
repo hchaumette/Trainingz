1cbaaacbe152621f66022ca1c7d43a2e0c8f8a4d
lucas regnier <lucas.regniergithub@gmail.com>
2022-12-01 17:25:19 +0100

    50% stimulus Exercise

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import SelectExerciseController from "./select_exercise_controller"
+application.register("select-exercise", SelectExerciseController)
