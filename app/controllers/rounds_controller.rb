aabb1c0f39b8b507e20ce21d262c99f45b0494e4
hugo chaumette <hugochaumette@gmail.com>
2022-12-02 16:22:42 +0100

    a lot a stimulus on round. Still have to managed ajax

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -4,7 +4,10 @@ class RoundsController < ApplicationController
     @round = Round.find(params[:id])
     @workout = @round.workout
     if @round.update(round_params)
-      redirect_to edit_workout_path(@round.workout)
+      respond_to do |format|
+        format.html { redirect_to edit_workout_path(@workout) }
+        format.text { render "workouts/edit", locals: { workout: @workout }, formats: [:html] }
+      end
     else
       render "workouts/edit", status: :unprocessable_entity
     end
