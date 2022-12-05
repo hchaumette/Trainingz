935c47395de5ebf7f41692d49c6d1e3266d29a90
hugo chaumette <hugochaumette@gmail.com>
2022-12-05 12:29:55 +0100

    added the created page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_090847) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_104610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_090847) do
   end
 
   create_table "rounds", force: :cascade do |t|
-    t.string "name"
+    t.string "name", default: "Round 1"
     t.bigint "workout_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
