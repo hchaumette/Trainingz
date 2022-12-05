63aa4f5d1efff927dd13960913bcdf3252cfc50c
hugo chaumette <hugochaumette@gmail.com>
2022-12-05 22:41:00 +0100

    little changes

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -2,8 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="share"
 export default class extends Controller {
-  static targets = ["form"]
-
   connect(){
   }
 
