571b33438b3784039ba6e679e8590c7ee15d67ef
Theo Mahoudo <theo.mah@hotmail.com>
2022-12-07 10:12:11 +0100

    avancement v4

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,9 @@ require 'openssl'
 require 'json'
 
 puts "Destroy all"
-puts"workout exercise"
+puts "workout exercise"
 WorkoutExercise.destroy_all
-puts"exercise"
+puts "exercise"
 Exercise.destroy_all
 puts "round"
 Round.destroy_all
@@ -18,19 +18,6 @@ puts "user"
 
 url = URI("https://exercisedb.p.rapidapi.com/exercises")
 
-puts "Create 4 users"
-user1 = User.create(email: "jean@wagon.org", password: "password", coach: true)
-user2 = User.create(email: "julien@wagon.org", password: "password")
-user3 = User.create(email: "etienne@mail.com", password: "password", coach: true)
-user4 = User.create(email: "frederic@gmail.fr", password: "password")
-puts "Done"
-puts "Create 4 workouts"
-workout1 = Workout.create(title: "Workout 1", user: user1)
-puts "Done"
-puts "Create 4 rounds "
-round1 = Round.create(name: "Premier", workout: workout1)
-puts "Done"
-WorkoutExercise.create(round: round1, exercise: Exercise.last)
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
@@ -47,13 +34,12 @@ exercises = JSON.parse(response.read_body)
 puts "Create 9 Exercises"
 
 exercises.first(9).each do |exercise|
-  durations = [12, 24, 32, 11, 10, 90]
+
   Exercise.create!(
     title: exercise["name"],
     equipment: exercise["equipment"],
     demonstration: exercise["gifUrl"],
     body_part: exercise["bodyPart"],
-    duration: durations.sample
   )
 end
 
